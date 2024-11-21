46bac50b4852cbdc1286a470dd55f144197a8f49
jonathan <jonathan03ant@gmail.com>
2024-11-21 14:56:54 -0500

    main demo

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World - This is the Main File")
+from Employee import Employee
+
+#Demo To Create The Employee
+#performance_metrics will initialize the metrics (percepts or sensors based on the passed dpt, and role)
+employee_1 = Employee(employee_id="001", 
+                      name="Emp_A", 
+                      department="HR", 
+                      role="HR",
+                      salary=50000)
+
+employee_1.print_employee()
