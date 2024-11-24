4c521234ef5c8bf2d8c266d7a254883c82551156
SammieJay <62515526+SammieJay@users.noreply.github.com>
2024-11-24 16:12:17 -0500

    Scroll Section Implemented

--- MainWindow.py
+++ MainWindow.py
@@ -1,7 +1,7 @@
 
 from PerformanceMetrics import Performance_metrics
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QListWidget, QScrollArea, QSplitter
 from PyQt6.QtCore import Qt
 
 class UI(QMainWindow):
@@ -9,20 +9,60 @@ class UI(QMainWindow):
 		super().__init__()
 
 		#Set Title of Window
-		self.setWindowTitle("Employee Assessment")
+		self.setWindowTitle("Employee Assessment Software")
 		
 		#Set Window Size
 		self.resize(800,600)
 
-		#Add Label
-		label= QLabel("Testing this out", self)
-		label.setStyleSheet("font-size: 24px;")
-		label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-		self.setCentralWidget(label)
+		#divide window
+		splitter = QSplitter(Qt.Orientation.Horizontal, self)
+
+		#Create Display Panel
+		view_panel = QWidget()
+		view_layout = QVBoxLayout()
+		
+		#Title For Viewing Panel
+		view_label = QLabel("Employee Evaluation")
+		view_label.setStyleSheet("font-size: 24px;")
+		view_layout.addWidget(view_label)
+		
+		view_panel.setLayout(view_layout)
+
+		# Scroll Panel And Content
+		select_panel = QWidget()
+		select_layout = QVBoxLayout()
+
+		scroll_area = QScrollArea()
+		scroll_area.setWidgetResizable(True)
+
+		scroll_content = QWidget()
+		scroll_content_layout = QVBoxLayout()
+
+		# Populate Scroll Panel
+		for i in range(150):
+			EmployeeID = f"{i:04}"
+			label = QLabel(f"Employee ID: {EmployeeID}")
+			scroll_content_layout.addWidget(label)
+			# print(f"Added Employee: {EmployeeID}")
+		scroll_content.setLayout(scroll_content_layout)
+		scroll_area.setWidget(scroll_content)
+
+		# Add Scroll Area to Layout
+		select_layout.addWidget(scroll_area)
+		select_panel.setLayout(select_layout)
+
+
+		# Add created panels to layout
+		splitter.addWidget(view_panel)
+		splitter.addWidget(select_panel)
+		splitter.setSizes([2,1])
+
+		self.setCentralWidget(splitter)
 
 
 
 
+# Run Window (will be called from a different file eventually)
 app = QApplication(sys.argv)
 window = UI()
 window.show()
