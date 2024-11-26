9aff6d5a25c0040e7f6ee554d909f213979ef304
SammieJay <62515526+SammieJay@users.noreply.github.com>
2024-11-26 17:40:00 -0500

    Added pycache to gitignore and added the ability to switch between a list of QWidget objects on the display

--- MainWindow.py
+++ MainWindow.py
@@ -1,12 +1,31 @@
 
 from PerformanceMetrics import Performance_metrics
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QListWidget, QScrollArea, QSplitter
+from PyQt6.QtWidgets import (
+	QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, 
+	QHBoxLayout, QLabel, QListWidget, QScrollArea, QSplitter,
+	QListWidget
+)
 from PyQt6.QtCore import Qt
 
 class UI(QMainWindow):
+	# ===== MEMBER VARIABLES =====
+	widget_array = []
+
+	view_panel = None
+	list_display = None
+	splitter = None
+
+
+
+	# ===== MEMBER FUNCITONS =====
+
 	def __init__(self):	#Function that runs when initialized
 		super().__init__()
+		# INITALIZE MEMBER Qt VARIABLES
+		self.view_panel = QWidget()
+		self.list_display = QListWidget()
+		self.splitter = QSplitter(Qt.Orientation.Horizontal, self)
 
 		#Set Title of Window
 		self.setWindowTitle("Employee Assessment Software")
@@ -14,11 +33,7 @@ class UI(QMainWindow):
 		#Set Window Size
 		self.resize(800,600)
 
-		#divide window
-		splitter = QSplitter(Qt.Orientation.Horizontal, self)
-
 		#Create Display Panel
-		view_panel = QWidget()
 		view_layout = QVBoxLayout()
 		
 		#Title For Viewing Panel
@@ -26,40 +41,60 @@ class UI(QMainWindow):
 		view_label.setStyleSheet("font-size: 24px;")
 		view_layout.addWidget(view_label)
 		
-		view_panel.setLayout(view_layout)
+		self.view_panel.setLayout(view_layout)
 
 		# Scroll Panel And Content
 		select_panel = QWidget()
 		select_layout = QVBoxLayout()
 
-		scroll_area = QScrollArea()
-		scroll_area.setWidgetResizable(True)
-
-		scroll_content = QWidget()
-		scroll_content_layout = QVBoxLayout()
-
-		# Populate Scroll Panel
+		# Populate Scroll Panel And Employee Array
 		for i in range(150):
 			EmployeeID = f"{i:04}"
-			label = QLabel(f"Employee ID: {EmployeeID}")
-			scroll_content_layout.addWidget(label)
+			self.list_display.addItem(f"ID: {EmployeeID}")
+			self.create_display_widget(f"ID: {EmployeeID}")
 			# print(f"Added Employee: {EmployeeID}")
-		scroll_content.setLayout(scroll_content_layout)
-		scroll_area.setWidget(scroll_content)
+		
+		#Connect to list_display slot
+		self.list_display.itemClicked.connect(self.on_item_clicked)
 
 		# Add Scroll Area to Layout
-		select_layout.addWidget(scroll_area)
+		select_layout.addWidget(self.list_display)
 		select_panel.setLayout(select_layout)
 
-
 		# Add created panels to layout
-		splitter.addWidget(view_panel)
-		splitter.addWidget(select_panel)
-		splitter.setSizes([2,1])
+		self.splitter.addWidget(self.view_panel)
+		self.splitter.addWidget(select_panel)
+		self.splitter.setSizes([2,1])
 
-		self.setCentralWidget(splitter)
+		self.setCentralWidget(self.splitter)
 
+	def create_display_widget(self, info):
+		widget = QWidget()
+		widget_layout = QVBoxLayout()
+		
+		# Populate Widget
+		label = QLabel(f"Display Info For Employee With {info}")
+		label.setStyleSheet("font-size: 24px;")
+		widget_layout.addWidget(label)
+		widget.setLayout(widget_layout)
 
+		# Add Widget to list
+		self.widget_array.append(widget)
+
+	
+	#Slot to handle item clicks for list panel
+	def on_item_clicked(self, item):
+		index = self.list_display.row(item)
+		#print(f"Selected Index: {index}, {item.text()}")
+		self.update_display(index)
+
+	# Update the displayed information on the display screen
+	def update_display(self, index):
+		#print(f"display update to show info for Employee with {item.text()}")
+		if index<self.widget_array.__sizeof__():
+			self.splitter.replaceWidget(0, self.widget_array[index])
+		else:
+			print("ERROR: Cannot Display Entree (Does Not Exist In widget_array)")
 
 
 # Run Window (will be called from a different file eventually)
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 - Samuel Abraham (101249272)
 - Jonathan Antenanie (101257486)
 - Hasin Ahmed (101181581)
+
+### Required Package Installations
+- PyQt6 for UI (Install by running: pip install PyQt6)
